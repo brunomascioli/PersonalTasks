bd9a9fe01daecbe935caaec2367c4f0ce4c4c705
brunomascioli <souzamasciolibruno@gmail.com>
2025-05-15 14:24:48 -0300

    feat: add Task class representing a task
    - include title, completion status, description, and due date

---
+++ app/src/main/java/com/example/personaltasks/Task.kt
@@ -0,0 +1,13 @@
+package com.example.personaltasks
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.time.LocalDateTime
+
+@Parcelize
+data class Task(
+    val title: String,
+    var isDone: Boolean = false,
+    val description: String,
+    val limitDate: LocalDateTime
+) : Parcelable
